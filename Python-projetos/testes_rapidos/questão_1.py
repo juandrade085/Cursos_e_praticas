8eacf15d99d41157045f75e7e822a2ce038280df
Juliana Andrade Souza <julianatechpt@gmail.com>
2025-06-14 19:52:58 +0100

    update

--- Python-projetos/testes_rapidos/questão_1.py
+++ Python-projetos/testes_rapidos/questão_1.py
@@ -3,4 +3,4 @@ número_inicial = int(input('Digite um número: '))
 while número_par <= número_inicial:
     print(número_par, end='.' if número_par + 1 >= número_inicial else ', ')
     número_par += 2
-print('\n--FIM--')
+print('\n---FIM---')
