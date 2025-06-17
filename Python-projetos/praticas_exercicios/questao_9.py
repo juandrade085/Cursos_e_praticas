e65caa805685b5f2828b5eac0d84f931f87de534
Juliana Andrade Souza <julianatechpt@gmail.com>
2025-06-17 12:43:41 +0100

    correction of the question

--- Python-projetos/praticas_exercicios/questao_9.py
+++ Python-projetos/praticas_exercicios/questao_9.py
@@ -1,6 +1,4 @@
-"""Crie um programa que peça um número inteiro ao usuário
-e determine se ele é primo. Use o loop while para verificar os
-divisores do número."""
+
 """Escreva um programa que exiba o seguinte padrão usando while:
 *
 **
