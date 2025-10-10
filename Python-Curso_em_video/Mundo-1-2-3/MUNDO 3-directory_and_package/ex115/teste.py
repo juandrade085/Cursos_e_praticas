820af43755c5864b598e843030f483d428d5b47e
Juliana Andrade Souza <julianatechpt@gmail.com>
2025-10-10 11:23:42 +0100

    college activity

---
+++ Python-Curso_em_video/Mundo-1-2-3/MUNDO 3-directory_and_package/ex115/teste.py
@@ -0,0 +1,63 @@
+prin("Tchau!")
+
+"""class Pessoa:
+    especie = 'Humano'
+
+    def __init__(self, nome):
+        self.nome = nome
+
+p1 = Pessoa('Alice')
+p2 = Pessoa ('Bob')
+
+p1.especie = 'Mutante'
+
+print(Pessoa.especie, p1.especie, p2.especie)
+
+
+x = 10
+def funcao1():
+    x = 20
+
+    def funcao2():
+        nonlocal x
+        x = 30
+        print(f"Valor de x dentro da função 2 é ",x)
+
+    funcao2()
+    print(f"Valor de x dentro da função 1 após a função2 ",x)
+
+funcao1()
+print(f"Valor de x no espaço global ",x)
+
+
+
+#Chamando uma função
+peso = float(input("Introduza o seu peso em KG: "))
+altura = float(input("Introduza a sua altura em metros (ex: 1.75): "))
+
+# Calcula o IMC usando a fórmula: peso / (altura ao quadrado)
+imc = peso / (altura ** 2)
+
+# Imprime o valor do IMC
+print(f"O seu IMC é: {imc:.2f}") # Limita a 2 casas decimais
+
+# Classifica o IMC
+if imc < 18.5:
+    print("Você está abaixo do peso.")
+elif imc < 25:
+    print("Você está com peso normal.")
+elif imc < 30:
+    print("Você está com sobrepeso.")
+else:
+    print("Você está com obesidade.")
+frutas = ["maça", "banana", "uva"]
+print("Lista de frutas:", frutas)
+frutas.append("Laranja")
+frutas.append("Morango")
+print("Após adicionar: ",frutas)
+
+print("Contando até 75 com while: ")
+contador = 1
+while contador <= 75:
+    print(contador)
+    contador += 1"""
